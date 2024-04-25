58ca97deb8d4989b103b8c8352abc1cc82e30f7a
Erkin <jumalieverkin2@gmail.com>
2024-04-26 01:51:10 +0600

    create method choisePay

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -1,8 +1,10 @@
 import enums.ActionLetter;
 import model.*;
+import org.w3c.dom.ls.LSOutput;
 import util.UniversalArray;
 import util.UniversalArrayImpl;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class AppRunner {
@@ -35,6 +37,7 @@ public class AppRunner {
     private void startSimulation() {
         print("В автомате доступны:");
         showProducts(products);
+        choiseMethodPay();
 
         print("Монет на сумму: " + coinAcceptor.getAmount());
 
@@ -91,6 +94,43 @@ public class AppRunner {
         for (int i = 0; i < products.size(); i++) {
             print(products.get(i).toString());
         }
+    }
+    public void choiseMethodPay() {
+        Random rnd = new Random();
+        System.out.print("Выберите способ оплаты:\n'c' или 'C' карта\n'm' или 'M' монета\n");
+        String userChoisePay = fromConsole();
+        while (true) {
+            if (userChoisePay.equalsIgnoreCase("c")) {
+                System.out.print("Введите номер карты без пробелов: ");
+                String userCard = fromConsole();
+                if (userCard.length() != 16) {
+                    print("Недопустимый номер карты, попробуйте еще раз");
+                } else {
+                    System.out.print("Введите пароль от карты: ");
+                    String userCardPassword = fromConsole();
+                    if (userCardPassword.length() < 8) {
+                        print("Пароль от карты должен быть не менее 8 символов!");
+                    } else {
+                        coinAcceptor.setAmount(rnd.nextInt(2 + (8)) * 10);
+                        break;
+                    }
+                }
+            } else if (userChoisePay.equalsIgnoreCase("m")) {
+                System.out.print("Сколько монет хотите закинуть?  ");
+                while (true) {
+                    try {
+                        int userCoinInt = Integer.parseInt(fromConsole());
+                        coinAcceptor.setAmount(userCoinInt);
+                        break;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Неверное значение для монет, попробуйте еще раз!");
+                    }
+                }
+                break;
+
+            }
+        }
+
     }
 
     private void print(String msg) {
